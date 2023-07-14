e839445cd88a75524ac0695e52d0fa6be62daa09
Carlosbil <carlosbilbao2@gmail.com>
2023-07-14 15:42:17 +0200

    Basic interfaze
    Basic interfaze that send the image and show the result

---
+++ interface/src/HomePage/HomePage.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import './HomePage.css';
+
+function HomePage() {
+  const [hovered, setHovered] = useState(false);
+  const [image, setImage] = useState(null);
+  const [prediction, setPrediction] = useState(null);
+  const [showToast, setShowToast] = useState(false);
+
+  const handleMouseEnter = () => {
+    setHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setHovered(false);
+  };
+
+  const handleImageUpload = event => {
+    setImage(URL.createObjectURL(event.target.files[0]));
+
+    const formData = new FormData();
+    formData.append('file', event.target.files[0]);
+
+    fetch('http://localhost:5000/predict', {
+      method: 'POST',
+      body: formData,
+    })
+    .then(response => response.json())
+    .then(data => {
+      setPrediction(data.prediction);
+      setShowToast(true);
+      setTimeout(() => setShowToast(false), 3000);  // Hides the toast after 3 seconds
+    })
+    .catch(error => console.error(error));
+  };
+
+  return (
+    <div className="container">
+      <h1
+        className={`welcome-text ${hovered ? "hovered" : ""}`}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        Welcome to this awesome world
+      </h1>
+      <input
+        type="file"
+        accept="image/*"
+        style={{ display: 'none' }}
+        id="fileInput"
+        onChange={handleImageUpload}
+      />
+      <label htmlFor="fileInput" className="futuristic-button">Submit to DogNet</label>
+      {showToast && (
+        <div className="toast">
+          {prediction}
+        </div>
+      )}
+      <div className="upload-section">
+        {image && <img src={image} alt="upload-preview" className="preview-image" />}
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
